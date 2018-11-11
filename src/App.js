d99467c126e7fced0f887d9dff2da4c1ce26fcf3
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-11 01:01:16 +0100

    Added routing to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component, Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import CryptoList from './components/CryptoList';
 import CustomAppBar from './components/common/CustomAppBar';
+import CryptoDetails from './components/CryptoDetails';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <CustomAppBar />
-        <CryptoList />
+        <Switch>
+          <Route path="/" exact component={CryptoList} />
+          <Route path="/coin/:id" component={CryptoDetails} />
+        </Switch>
       </Fragment>
     );
   }

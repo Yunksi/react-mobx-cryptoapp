65f671cb3ed74940e3d8f0d328e86e431cdfe148
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-11 19:59:36 +0100

    Added routing to appbar

--- src/components/common/CustomAppBar.js
+++ src/components/common/CustomAppBar.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import ToolbarMenu from './ToolbarMenu';
+import { Link } from 'react-router-dom';
 
 export class CustomAppBar extends Component {
   render() {
     return (
-      <div>
-        <AppBar position="static" color="primary">
+      <div style={{ flexGrow: 1 }}>
+        <AppBar position="static">
           <Toolbar>
-            <Typography variant="h6" color="inherit">
-              CryptoApp
-            </Typography>
+            <Link
+              to="/"
+              style={{ flexGrow: 1, color: 'white', textDecoration: 'none' }}
+            >
+              <Typography variant="h6" color="inherit" style={{ flexGrow: 1 }}>
+                CryptoApp
+              </Typography>
+            </Link>
+
+            <ToolbarMenu />
           </Toolbar>
         </AppBar>
       </div>

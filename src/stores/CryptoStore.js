aabcca64666ecfb4c0af0fa910303fc87f02314b
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-11 19:58:24 +0100

    Changed API url to CMC v2

--- src/stores/CryptoStore.js
+++ src/stores/CryptoStore.js
@@ -1,17 +1,21 @@
 import { observable, configure, decorate, action, runInAction } from 'mobx';
 import Axios from 'axios';
 
-const BASE_API_URL = 'https://api.coinmarketcap.com/v1';
+const BASE_API_URL = 'https://api.coinmarketcap.com/v2';
 
 configure({ enforceActions: 'observed' });
 
 class CryptoStore {
   coins = [];
   isLoadingCoins = true;
+  anchorEl = null;
+  currency = 'USD';
 
   fetchCoinData = () => {
     this.isLoadingCoins = true;
-    Axios.get(`${BASE_API_URL}/ticker/`).then(res => {
+    Axios.get(
+      `${BASE_API_URL}/ticker/?convert=${this.currency}&structure=array`
+    ).then(res => {
       runInAction(() => {
         this.setCoins(res.data);
       });
@@ -19,15 +23,45 @@ class CryptoStore {
   };
 
   setCoins = data => {
-    this.coins = data;
+    this.coins = data.data;
+    console.log(data.data);
     this.isLoadingCoins = false;
+  };
+
+  openMenu = e => {
+    this.anchorEl = e;
+  };
+
+  closeMenu = selectedCurrency => {
+    switch (selectedCurrency) {
+      case 'USD':
+        this.currency = 'USD';
+        this.fetchCoinData();
+        break;
+      case 'EUR':
+        this.currency = 'EUR';
+        this.fetchCoinData();
+        break;
+      case 'CNY':
+        this.currency = 'CNY';
+        this.fetchCoinData();
+        break;
+      default:
+        break;
+    }
+    this.anchorEl = null;
   };
 }
 
 decorate(CryptoStore, {
   coins: observable,
   isLoadingCoins: observable,
-  fetchCoinData: action
+  isMenuOpen: observable,
+  anchorEl: observable,
+  currency: observable,
+  fetchCoinData: action,
+  openMenu: action,
+  closeMenu: action
 });
 
 var store = (window.store = new CryptoStore());

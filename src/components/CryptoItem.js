37f7988a47f5e4fbf37eba746fce7cfc5aa374fc
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-11 20:00:02 +0100

    Updated CryptoItem component to v2 api, added coin images and 7d chart

--- src/components/CryptoItem.js
+++ src/components/CryptoItem.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { TableRow, TableCell } from '@material-ui/core';
+import { TableRow, TableCell, Hidden } from '@material-ui/core';
 import { withRouter } from 'react-router';
+import './CryptoItem.css';
 
 export class CryptoItem extends Component {
   goToDetails = () => {
@@ -9,15 +10,58 @@ export class CryptoItem extends Component {
   };
 
   render() {
-    const { coin } = this.props;
+    const { coin, currency } = this.props;
+    let priceSign = '$';
+    switch (currency) {
+      case 'USD':
+        priceSign = '$';
+        break;
+      case 'EUR':
+        priceSign = '€';
+        break;
+      case 'CNY':
+        priceSign = '¥';
+        break;
+      default:
+        break;
+    }
+    const coinLogo = `https://s2.coinmarketcap.com/static/img/coins/16x16/${
+      coin.id
+    }.png`;
+    const coinPriceGraph7d = `https://s2.coinmarketcap.com/generated/sparklines/web/7d/usd/${
+      coin.id
+    }.png`;
+
     return (
       <TableRow onClick={this.goToDetails}>
         <TableCell component="th" scope="row">
           {coin.rank}
         </TableCell>
-        <TableCell>{coin.symbol}</TableCell>
-        <TableCell>{coin.price_usd}</TableCell>
-        <TableCell>{coin.percent_change_24h}</TableCell>
+        <TableCell>
+          <div className="coinSymbol">
+            <img src={coinLogo} alt="Coin Logo" className="logo-sprite" />
+            {coin.symbol}
+          </div>
+        </TableCell>
+        <TableCell>
+          {priceSign}
+          {parseFloat(coin.quotes[currency].price).toFixed(2)}
+        </TableCell>
+        <Hidden xsDown>
+          <TableCell
+            style={{
+              color:
+                coin.quotes[currency].percent_change_24h > 0 ? 'green' : 'red'
+            }}
+          >
+            {coin.quotes[currency].percent_change_24h}%
+          </TableCell>
+        </Hidden>
+        <Hidden smDown>
+          <TableCell>
+            <img src={coinPriceGraph7d} alt="Coin Sparkline" />
+          </TableCell>
+        </Hidden>
       </TableRow>
     );
   }

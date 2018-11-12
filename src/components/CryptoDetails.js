7d8501c190ca985080e57435a71f25a904814ee1
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-12 21:57:47 +0100

    Added refresh button to details view

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { inject, observer } from 'mobx-react';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import CoinMarketInfo from './CoinMarketInfo';
 import CoinValueInfo from './CoinValueInfo';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
 export class CryptoDetails extends Component {
   componentDidMount = () => {
+    const coinId = this.props.match.params.id;
+    this.props.cryptoStore.fetchSingleCoin(coinId);
+    this.props.cryptoStore.fetchSingleCoinBTCPrice(coinId);
+  };
+
+  refreshCoin = () => {
     this.props.cryptoStore.fetchSingleCoin(this.props.match.params.id);
-    this.props.cryptoStore.fetchSingleCoinBTCPrice(this.props.match.params.id);
   };
 
   render() {
@@ -25,36 +32,53 @@ export class CryptoDetails extends Component {
     return (
       <div style={{ marginLeft: '0.5em', marginRight: '0.5em' }}>
         {!loadingCoinDetails ? (
-          <Grid container spacing={8}>
-            <Grid item xs={12} sm={6}>
-              <div
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                  marginLeft: '0.5em'
-                }}
+          <Fragment>
+            <Grid
+              container
+              direction="row"
+              justify="flex-end"
+              style={{ marginTop: '1em' }}
+            >
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={this.refreshCoin}
               >
-                <img
-                  src={coinLogo}
-                  alt="coin logo"
-                  height="32"
-                  width="32"
-                  style={{ marginRight: '4px' }}
-                />
-                <h1>
-                  {selectedCoin.name} ({selectedCoin.symbol}) #
-                  {selectedCoin.rank}
-                </h1>
-              </div>
+                Refresh
+                <Icon>refresh</Icon>
+              </Button>
             </Grid>
-            <CoinValueInfo
-              btc={coinBTCData}
-              currency={currency}
-              coin={selectedCoin}
-            />
-            <CoinMarketInfo currency={currency} coin={selectedCoin} />
-          </Grid>
+            <Grid container spacing={8}>
+              <Grid item xs={12} sm={6}>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    marginLeft: '0.5em'
+                  }}
+                >
+                  <img
+                    src={coinLogo}
+                    alt="coin logo"
+                    height="32"
+                    width="32"
+                    style={{ marginRight: '4px' }}
+                  />
+                  <h1>
+                    {selectedCoin.name} ({selectedCoin.symbol}) #
+                    {selectedCoin.rank}
+                  </h1>
+                </div>
+              </Grid>
+              <CoinValueInfo
+                btc={coinBTCData}
+                currency={currency}
+                coin={selectedCoin}
+              />
+              <CoinMarketInfo currency={currency} coin={selectedCoin} />
+            </Grid>
+          </Fragment>
         ) : (
           <div>
             <CircularProgress />

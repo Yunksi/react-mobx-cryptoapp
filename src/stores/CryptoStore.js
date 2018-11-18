2bc57c755aff8f62f6ea2e7c11f87ce0bbde86c2
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-18 19:53:46 +0100

    Refactored store to async/await

--- src/stores/CryptoStore.js
+++ src/stores/CryptoStore.js
@@ -1,10 +1,10 @@
 import {
-  observable,
-  configure,
-  decorate,
-  action,
-  runInAction,
-  computed
+	observable,
+	configure,
+	decorate,
+	action,
+	runInAction,
+	computed
 } from 'mobx';
 import Axios from 'axios';
 
@@ -13,106 +13,105 @@ const BASE_API_URL = 'https://api.coinmarketcap.com/v2';
 configure({ enforceActions: 'observed' });
 
 class CryptoStore {
-  coins = [];
-  isLoadingCoins = true;
-  anchorEl = null;
-  currency = 'USD';
-  selectedCoin = null;
-  loadingCoinDetails = true;
-  coinBTCData = null;
+	coins = [];
+	isLoadingCoins = true;
+	anchorEl = null;
+	currency = 'USD';
+	selectedCoin = null;
+	loadingCoinDetails = true;
+	coinBTCData = null;
 
-  fetchCoinData = () => {
-    this.isLoadingCoins = true;
-    Axios.get(
-      `${BASE_API_URL}/ticker/?convert=${this.currency}&structure=array`
-    ).then(res => {
-      runInAction(() => {
-        this.setCoins(res.data);
-      });
-    });
-  };
+	async fetchCoinData() {
+		this.isLoadingCoins = true;
+		const coins = await Axios.get(
+			`${BASE_API_URL}/ticker/?convert=${this.currency}&structure=array`
+		);
+		runInAction(() => {
+			this.setCoins(coins.data);
+		});
+	}
 
-  fetchSingleCoin = coinId => {
-    this.loadingCoinDetails = true;
-    Axios.get(
-      `${BASE_API_URL}/ticker/${coinId}/?convert=${this.currency}`
-    ).then(res => {
-      runInAction(() => {
-        this.setCoin(res.data.data);
-      });
-    });
-  };
+	async fetchSingleCoin(coinId) {
+		this.loadingCoinDetails = true;
+		var coin = await Axios.get(
+			`${BASE_API_URL}/ticker/${coinId}/?convert=${this.currency}`
+		);
+		runInAction(() => {
+			this.setCoin(coin.data.data);
+		});
+	}
 
-  fetchSingleCoinBTCPrice = coinId => {
-    Axios.get(`${BASE_API_URL}/ticker/${coinId}/?convert=BTC`).then(res => {
-      runInAction(() => {
-        this.setCoinBTCdata(res.data.data.quotes.BTC);
-      });
-    });
-  };
+	async fetchSingleCoinBTCPrice(coinId) {
+		var btcCoin = await Axios.get(
+			`${BASE_API_URL}/ticker/${coinId}/?convert=BTC`
+		);
+		runInAction(() => {
+			this.setCoinBTCdata(btcCoin.data.data.quotes.BTC);
+		});
+	}
 
-  setCoinBTCdata = data => {
-    this.coinBTCData = data;
-  };
+	setCoinBTCdata = data => {
+		this.coinBTCData = data;
+	};
 
-  setCoin = data => {
-    this.selectedCoin = data;
-    this.loadingCoinDetails = false;
-  };
+	setCoin = data => {
+		this.selectedCoin = data;
+		this.loadingCoinDetails = false;
+	};
 
-  setCoins = data => {
-    this.coins = data.data;
-    this.isLoadingCoins = false;
-  };
+	setCoins = data => {
+		this.coins = data.data;
+		this.isLoadingCoins = false;
+	};
 
-  openMenu = e => {
-    this.anchorEl = e;
-  };
+	openMenu = e => {
+		this.anchorEl = e;
+	};
 
-  closeMenu = selectedCurrency => {
-    switch (selectedCurrency) {
-      case 'USD':
-        this.currency = 'USD';
-        break;
-      case 'EUR':
-        this.currency = 'EUR';
-        break;
-      case 'CNY':
-        this.currency = 'CNY';
-        break;
-      default:
-        break;
-    }
-    this.anchorEl = null;
-    this.fetchCoinData();
-    if (this.selectedCoin) {
-      this.fetchSingleCoin(this.selectedCoin.id);
-    }
-  };
+	closeMenu = selectedCurrency => {
+		switch (selectedCurrency) {
+			case 'USD':
+				this.currency = 'USD';
+				break;
+			case 'EUR':
+				this.currency = 'EUR';
+				break;
+			case 'CNY':
+				this.currency = 'CNY';
+				break;
+			default:
+				break;
+		}
+		this.anchorEl = null;
+		this.fetchCoinData();
+		if (this.selectedCoin) {
+			this.fetchSingleCoin(this.selectedCoin.id);
+		}
+	};
 
-  get percentChangeClass() {
-    if (this.selectedCoin)
-      return this.selectedCoin.quotes[this.currency].percent_change_24h > 0
-        ? 'green'
-        : 'red';
-    return 'red';
-  }
+	get percentChangeClass() {
+		if (this.selectedCoin)
+			return this.selectedCoin.quotes[this.currency].percent_change_24h > 0
+				? 'green'
+				: 'red';
+		return 'red';
+	}
 }
 
 decorate(CryptoStore, {
-  coins: observable,
-  isLoadingCoins: observable,
-  isMenuOpen: observable,
-  anchorEl: observable,
-  currency: observable,
-  selectedCoin: observable,
-  loadingCoinDetails: observable,
-  coinBTCData: observable,
-  fetchCoinData: action,
-  openMenu: action,
-  closeMenu: action,
-  fetchSingleCoin: action,
-  percentChangeClass: computed
+	coins: observable,
+	isLoadingCoins: observable,
+	isMenuOpen: observable,
+	anchorEl: observable,
+	currency: observable,
+	selectedCoin: observable,
+	loadingCoinDetails: observable,
+	coinBTCData: observable,
+	fetchCoinData: action,
+	openMenu: action,
+	closeMenu: action,
+	fetchSingleCoin: action,
+	percentChangeClass: computed
 });
 
 var store = new CryptoStore();
